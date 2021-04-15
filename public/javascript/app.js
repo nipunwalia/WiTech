44c9f78ba003734c8fe34fb2c6d7006a9dbed41f
Brinda Kaushik <brinda8411@gmail.com>
2021-04-15 17:33:15 +0530

    added about and testimonials

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -7,3 +7,22 @@ window.addEventListener('scroll', function () {
         nav.classList.remove('bg-dark', 'shadow');
     }
 });
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("carousel-item1")
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000);
+}
