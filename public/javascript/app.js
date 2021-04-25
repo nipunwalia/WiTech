4f87d652d9619b0d75d8fe75526f50d81cebc06d
Brinda Kaushik <brinda8411@gmail.com>
2021-04-25 15:38:24 +0530

    image slideshow,covid-19 option,and assets added

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,6 +1,25 @@
 $(window).scroll(function(){
 	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+slideshows.forEach(initSlideShow);
+
+function initSlideShow(slideshow) {
+
+  var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+
+  var index = 0, time = 3000;
+  slides[index].classList.add('active');  
+  
+  setInterval( () => {
+    slides[index].classList.remove('active');
+    index++;
+    if (index === slides.length) index = 0; 
+    
+    slides[index].classList.add('active');
+
+  }, time);
+}
 var slideIndex = 0;
 showSlides();
 function showSlides() {
