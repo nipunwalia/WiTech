ef54330c0512ad91cdb6d0ef8ace35509c8ee1cc
Jatin Agrawal <jatinagrawal0801@gmail.com>
2021-04-26 22:59:02 +0530

    morgan middleware added

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ const mongoose=require('mongoose');
 const formRouter=require('./routes/formRouter');
 const departmentRouter=require('./routes/departmentRouter');
 const cors=require('cors');
+const morgan = require('morgan');
 const app=express();
 const port=5000;
 
+// stateslist for volunteer form
 var statesList=['AndhraPradesh','ArunachalPradesh','Assam','Bihar','Chhattisgarh','Goa','Gujarat','Haryana','HimachalPradesh',
     'Jharkhand','Karnataka','Kerala','MadhyaPradesh','Maharashtra','Manipur','Meghalaya','Mizoram','Nagaland',
     'Odisha','Punjab','Rajasthan','Sikkim','TamilNadu','Telangana','Tripura','UttarPradesh','Uttarakhand','WestBengal'
 ];
 
 app.use(cors());
+app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-mongoose.connect('mongodb://localhost/witech', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost/witech', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(express.static('public'));
 app.set('view engine','ejs');
