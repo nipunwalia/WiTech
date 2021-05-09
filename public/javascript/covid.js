c02031481f79ad9f545540faa40955c267397d91
Brinda Kaushik <brinda8411@gmail.com>
2021-05-09 15:47:59 +0530

    news section added

--- public/javascript/covid.js
+++ public/javascript/covid.js
@@ -1,3 +1,26 @@
+$(window).scroll(function(){
+	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+slideshows.forEach(initSlideShow);
+
+function initSlideShow(slideshow) {
+
+  var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+
+  var index = 0, time = 3000;
+  slides[index].classList.add('active');  
+  
+  setInterval( () => {
+    slides[index].classList.remove('active');
+    index++;
+    if (index === slides.length) index = 0; 
+    
+    slides[index].classList.add('active');
+
+  }, time);
+}
+
 async function getCovidData(){
     var xhttp=new XMLHttpRequest();
     xhttp.open("GET","/api/covid/getData",true);
