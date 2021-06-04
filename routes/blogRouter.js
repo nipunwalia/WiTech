199c641aa1da9272aa055a54fb128ac923f923d9
Jatin Agrawal <jatinagrawal0801@gmail.com>
2021-06-04 21:20:04 +0530

    tags and categories configured

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -6,32 +6,32 @@ const {blogData}=require('./data')
 // route to list all blogs
 blogRouter.get('/',async(req,res)=>{
     var data=await Blog.find({});
-    res.render('blog',{blogData:data});
+    let category=req.query.category || "";
+    let tags=req.query.tags || "";
+    res.render('blog',{blogData:data,category:category,tags:tags});
 });
 
 // route for a single blog page
 // this method is only for current settings
 // if anyone can post a blog on this website then we need to change this and make this unique.
 blogRouter.get('/:id',async(req,res)=>{
-    let allData=await await Blog.find({});
+    let allData= await Blog.find({});
     let data=allData.find(element=>element.blogid == req.params.id);
     res.render('blog-single',{blogData:data,recentPosts:allData});
 });
 
 
-// for time being we can assume only we will add that blogs manually.
-// from id we have to fetch that blog from db
-// for that we need to set a manual userid for that particualr blog.
-// generting user/blogid remaining.
-// creating a new blog
 blogRouter.post('/api/create',async(ren,res)=>{
-    // blogData[0].blogid=
-    var req={body:blogData[5]};
+    // var req={body:blogData[6]};
     var result=new Blog({blogid:req.body.blogid,title:req.body.title,author:req.body.author,date:req.body.date,
-        image:req.body.image,about:req.body.about,category:req.body.category,content:req.body.content,
+        image:req.body.image,about:req.body.about,category:req.body.category,tags:req.body.tags,content:req.body.content,
         comments:req.body.comments});
     const blog=result.save();
-    res.send("Saved");
+    if(blog){
+        res.send('Saved');
+    }else{
+        res.status(404).send("Error");
+    }
 });
 
 // route for posting comment.
